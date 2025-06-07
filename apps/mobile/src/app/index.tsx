f48b7937a167162b4ecea693af2c923eb156c32a
lancej1022 <46359098+lancej1022@users.noreply.github.com>
2025-06-07 12:28:41 -0700

    Add React-Native-Reusables to `mobile`

--- apps/mobile/src/app/index.tsx
+++ apps/mobile/src/app/index.tsx
@@ -3,122 +3,7 @@ import { Text } from "react-native";
 import { SafeAreaView } from "react-native-safe-area-context";
 import { Stack } from "expo-router";
 
-// import type { RouterOutputs } from "~/utils/api";
-// import { trpc } from "~/utils/api";
-// import { authClient } from "~/utils/auth";
-
-// function PostCard(props: {
-//   post: RouterOutputs["post"]["all"][number];
-//   onDelete: () => void;
-// }) {
-//   return (
-//     <View className="flex flex-row rounded-lg bg-muted p-4">
-//       <View className="flex-grow">
-//         <Link
-//           asChild
-//           href={{
-//             pathname: "/post/[id]",
-//             params: { id: props.post.id },
-//           }}
-//         >
-//           <Pressable className="">
-//             <Text className="text-xl font-semibold text-primary">
-//               {props.post.title}
-//             </Text>
-//             <Text className="mt-2 text-foreground">{props.post.content}</Text>
-//           </Pressable>
-//         </Link>
-//       </View>
-//       <Pressable onPress={props.onDelete}>
-//         <Text className="font-bold uppercase text-primary">Delete</Text>
-//       </Pressable>
-//     </View>
-//   );
-// }
-
-// function CreatePost() {
-//   const queryClient = useQueryClient();
-
-//   const [title, setTitle] = useState("");
-//   const [content, setContent] = useState("");
-
-//   const { mutate, error } = useMutation(
-//     trpc.post.create.mutationOptions({
-//       async onSuccess() {
-//         setTitle("");
-//         setContent("");
-//         await queryClient.invalidateQueries(trpc.post.all.queryFilter());
-//       },
-//     }),
-//   );
-
-//   return (
-//     <View className="mt-4 flex gap-2">
-//       <TextInput
-//         className="items-center rounded-md border border-input bg-background px-3 text-lg leading-[1.25] text-foreground"
-//         value={title}
-//         onChangeText={setTitle}
-//         placeholder="Title"
-//       />
-//       {error?.data?.zodError?.fieldErrors.title && (
-//         <Text className="mb-2 text-destructive">
-//           {error.data.zodError.fieldErrors.title}
-//         </Text>
-//       )}
-//       <TextInput
-//         className="items-center rounded-md border border-input bg-background px-3 text-lg leading-[1.25] text-foreground"
-//         value={content}
-//         onChangeText={setContent}
-//         placeholder="Content"
-//       />
-//       {error?.data?.zodError?.fieldErrors.content && (
-//         <Text className="mb-2 text-destructive">
-//           {error.data.zodError.fieldErrors.content}
-//         </Text>
-//       )}
-//       <Pressable
-//         className="flex items-center rounded bg-primary p-2"
-//         onPress={() => {
-//           mutate({
-//             title,
-//             content,
-//           });
-//         }}
-//       >
-//         <Text className="text-foreground">Create</Text>
-//       </Pressable>
-//       {error?.data?.code === "UNAUTHORIZED" && (
-//         <Text className="mt-2 text-destructive">
-//           You need to be logged in to create a post
-//         </Text>
-//       )}
-//     </View>
-//   );
-// }
-
-// function MobileAuth() {
-//   const { data: session } = authClient.useSession();
-
-//   return (
-//     <>
-//       <Text className="pb-2 text-center text-xl font-semibold text-white">
-//         {session?.user.name ?? "Not logged in"}
-//       </Text>
-//       <Button
-//         onPress={() =>
-//           session
-//             ? authClient.signOut()
-//             : authClient.signIn.social({
-//                 provider: "discord",
-//                 callbackURL: "/",
-//               })
-//         }
-//         title={session ? "Sign Out" : "Sign In With Discord"}
-//         color={"#5B65E9"}
-//       />
-//     </>
-//   );
-// }
+import { Button } from "~/components/ui/button";
 
 export default function Index() {
   // const queryClient = useQueryClient();
@@ -165,6 +50,9 @@ export default function Index() {
         <CreatePost />
       </View> */}
       <Text>Hello world</Text>
+      <Button>
+        <Text>Click me</Text>
+      </Button>
     </SafeAreaView>
   );
 }
