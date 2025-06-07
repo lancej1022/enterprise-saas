a160322606b7b6b24e5690dd232847d2a022577e
lancej1022 <46359098+lancej1022@users.noreply.github.com>
2025-06-07 11:40:35 -0700

    Additional eslint config tweaks

--- tooling/eslint/base.js
+++ tooling/eslint/base.js
@@ -1,12 +1,21 @@
 /// <reference types="./types.d.ts" />
 
 import { join } from "node:path";
+// @ts-expect-error https://github.com/eslint-community/eslint-plugin-eslint-comments/issues/214
+import ESLintPluginESLintCommentsConfigs from "@eslint-community/eslint-plugin-eslint-comments/configs";
 import { includeIgnoreFile } from "@eslint/compat";
 import eslint from "@eslint/js";
 import importPlugin from "eslint-plugin-import";
 import turboPlugin from "eslint-plugin-turbo";
 import unusedImports from "eslint-plugin-unused-imports";
 import tseslint from "typescript-eslint";
+
+// The plugin is not currently exported from the root, so we have to get the plugin from the config.
+// https://github.com/eslint-community/eslint-plugin-eslint-comments/issues/215#issuecomment-2140949371
+const ESLintPluginESLintComments =
+  ESLintPluginESLintCommentsConfigs.recommended.plugins[
+    "@eslint-community/eslint-comments"
+  ];
 
 /**
  * All packages that leverage t3-env should use this rule
@@ -48,6 +57,7 @@ export default tseslint.config(
       import: importPlugin,
       "unused-imports": unusedImports,
       turbo: turboPlugin,
+      "@eslint-community/eslint-comments": ESLintPluginESLintComments,
     },
     extends: [
       eslint.configs.recommended,
@@ -60,10 +70,19 @@ export default tseslint.config(
       "@typescript-eslint/no-unused-vars": "off",
       // provides an autofixable version of no-unused-vars, for imports specifically
       "unused-imports/no-unused-imports": "error",
+      // this rule improves tree-shaking by ensuring types are consistently imported in a way that allows them to be removed from production bundles
       "@typescript-eslint/consistent-type-imports": [
         "warn",
         { prefer: "type-imports", fixStyle: "inline-type-imports" },
       ],
+      "@typescript-eslint/consistent-type-assertions": [
+        "error",
+        { assertionStyle: "never" },
+      ],
+      // https://www.totaltypescript.com/react-apps-ts-performance -- `interface` is more performant during type-checking compared to `type`, especially in large codebases
+      "@typescript-eslint/consistent-type-definitions": ["error", "interface"],
+      // Explicit type annotations add unnecessary verbosity to code and in some cases can prevent TypeScript from inferring a more specific literal type (e.g. `10` instead `number`)
+      "@typescript-eslint/no-inferrable-types": "error",
       "@typescript-eslint/no-misused-promises": [
         "error",
         { checksVoidReturn: { attributes: false } },
@@ -83,6 +102,13 @@ export default tseslint.config(
           message: "Use `import { z } from 'zod/v4'` instead to ensure v4.",
         },
       ],
+      "@eslint-community/eslint-comments/require-description": [
+        "error",
+        { ignore: [] },
+      ],
+      "no-console": ["error", { allow: ["error"] }],
+      // improves perf and reduces bugs by ensuring devs dont write code where the operators behave differently from the way the developer thought they would
+      "no-constant-binary-expression": "error",
     },
   },
   {
