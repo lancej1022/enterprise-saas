a15b2e117f5b48e91419b24c66d55b1ba6c9c746
lancej1022 <46359098+lancej1022@users.noreply.github.com>
2025-07-04 17:27:31 -0700

    More shadcn monorepo wip

--- apps/web/src/routes/-components/login-form.tsx
+++ apps/web/src/routes/-components/login-form.tsx
@@ -3,7 +3,6 @@ import { useAppForm } from "@/components/tanstack-form";
 import { Input } from "@/components/ui/input";
 import { Label } from "@/components/ui/label";
 import { authClient } from "@/lib/auth-client";
-import { cn } from "@/lib/utils";
 import {
   Link,
   useLocation,
@@ -15,6 +14,7 @@ import { toast } from "sonner";
 import { z } from "zod/v4";
 
 import { Button } from "@acme/ui/components/button";
+import { cn } from "@acme/ui/lib/utils";
 
 const loginSchema = z.object({
   email: z.string().email("Invalid email address"),
